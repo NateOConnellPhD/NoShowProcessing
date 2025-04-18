2f403a7b311c88418aba22eab2689188de9483e3
Nate OConnell <oconrunner@gmail.com>
2025-04-18 07:50:43 -0400

    update post_clean and save_master

--- R/new_master.R
+++ R/new_master.R
@@ -1,22 +1,18 @@
-new_master <- function(master_today) {
-  today <- Sys.Date()
-  dateformat <- format(today, "%Y%m%d")
-  
+new_master <- function(master_today, master) {
+
   master_file <- "processed_data/master.rda"
-  new_master_path <- paste0("processed_data/", dateformat, "_master.rda")
-  
+
   if (file.exists(master_file)) {
-    old_master <- readRDS(master_file)
-    
+
     # Merge eligibles and full (data frames)
-    eligibles <- bind_rows(old_master$eligibles, master_today$eligibles)
-    full <- post_clean(master_today, old_master)
-    
+    eligibles <- bind_rows(master$eligibles, master_today$eligibles)
+    full <- master$full
+
     # Merge inel components individually
-    inel_keys <- union(names(old_master$inel), names(master_today$inel))
+    inel_keys <- union(names(master$inel), names(master_today$inel))
     inel <- setNames(vector("list", length(inel_keys)), inel_keys)
     for (key in inel_keys) {
-      df_old <- old_master$inel[[key]]
+      df_old <- master$inel[[key]]
       df_new <- master_today$inel[[key]]
       if (!is.null(df_old) && !is.null(df_new)) {
         inel[[key]] <- bind_rows(df_old, df_new)
@@ -26,20 +22,58 @@ new_master <- function(master_today) {
         inel[[key]] <- df_new
       }
     }
-    
+
   } else {
     eligibles <- master_today$eligibles
     inel <- master_today$inel
-    full <- post_clean(master_today, master_today)
+    full <- master$full
   }
-  
+
   list(eligibles = eligibles, inel = inel, full = full)
 }
 
 
 
-
-
-
-
-
+# new_master <- function(master_today, master, date) {
+#   dateformat <- date
+#   master_file <- "processed_data/master.rda"
+#   new_master_path <- paste0("processed_data/", dateformat, "_master.rda")
+#
+#   if (file.exists(master_file)) {
+#
+#
+#     # Merge eligibles and full (data frames)
+#     eligibles <- bind_rows(old_master$eligibles, master_today$eligibles)
+#     full <- post_clean(master_today, old_master)
+#
+#     # Merge inel components individually
+#     inel_keys <- union(names(old_master$inel), names(master_today$inel))
+#     inel <- setNames(vector("list", length(inel_keys)), inel_keys)
+#     for (key in inel_keys) {
+#       df_old <- old_master$inel[[key]]
+#       df_new <- master_today$inel[[key]]
+#       if (!is.null(df_old) && !is.null(df_new)) {
+#         inel[[key]] <- bind_rows(df_old, df_new)
+#       } else if (!is.null(df_old)) {
+#         inel[[key]] <- df_old
+#       } else if (!is.null(df_new)) {
+#         inel[[key]] <- df_new
+#       }
+#     }
+#
+#   } else {
+#     eligibles <- master_today$eligibles
+#     inel <- master_today$inel
+#     full <- post_clean(master_today, master_today)
+#   }
+#
+#   list(eligibles = eligibles, inel = inel, full = full)
+# }
+#
+#
+#
+#
+#
+#
+#
+#
